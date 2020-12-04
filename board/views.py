e737f56e38a155849bd092836740e80c58768126
JiAn-LIM <jian1990@mailplug.co.kr>
2020-12-04 11:44:14 +0900

    testtesttest

--- board/views.py
+++ board/views.py
@@ -64,7 +64,7 @@ class CommentUpdate(UpdateView):
     template_name = 'comment_update.html'
     article = Article()
     # success_url = '/'
-
+    # qwerqwerqwer
     def dispatch(self, request, *args, **kwargs):
         object = self.get_object()
         if object.author != request.user:
